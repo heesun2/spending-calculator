945c2437cec59389ba09e7c5398e12546a01e9fe
02.dom <designh2sun@gmail.com>
2024-03-12 20:54:17 +0900

    React calculator

---
+++ src/components/ExpenseList.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+import React, { Component } from 'react'
+import './ExpenseList.css'
+import ExpenseItem from './ExpenseItem'
+import{MdDelete} from 'react-icons/md';
+
+export class ExpenseList extends Component {
+  render() {
+    console.log(this.props.initialExpenses)
+    return (
+      <>
+        <ul className='list'>
+          {this.props.initialExpenses.map(expense => {
+            return(
+              <ExpenseItem 
+              expense={expense}
+              key = {expense.id}
+              handleDelete={this.props.handleDelete}
+              />
+            )
+          })}
+        </ul>
+        <button className='btn'>
+          목록지우기
+          <MdDelete className='btn-icon'/>
+        </button>
+      </>
+    )
+  }
+}
+
+export default ExpenseList
